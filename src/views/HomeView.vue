20a735b4e829980045117d0b07b9ec6431ce86f5
Lars Wagner <lars@tubies.de>
2024-04-17 20:46:05 +0200

    my first commit ;)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@
     <div>Hello Track Stack</div>
     <div>Hallo von Martin!</div>
     <div>Auch ein Hallo von Irina! 😁</div>
+    <div>Der Lars ist auch dabei! :-P</div>
 
     <router-link to="/trackstack"><button>LandingPage</button></router-link>
   </main>
