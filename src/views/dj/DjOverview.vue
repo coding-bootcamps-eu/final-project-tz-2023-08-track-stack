1533c32cb0072d7e187cec7724bcf6a8d79f3712
Martin Schneider <schnmart86@gmail.com>
2024-05-04 18:25:28 +0200

    feat: first Login without api

--- src/views/dj/DjOverview.vue
+++ src/views/dj/DjOverview.vue
@@ -1,6 +1,10 @@
 <template>
   <small>DjOverview</small>
-  <h2>Übersicht DJ</h2>
+
+  <h2>
+    Übersicht DJ
+    <span id="loggedDjWelcome">{{ loggedDj }}</span>
+  </h2>
   <p>Hier bist du auf deinem Dashboard</p>
   <div class="grid">
     <router-link to="/create-event"><button>Event erstellen</button></router-link>
@@ -17,3 +21,30 @@
     <router-link to="/wishlist"><button class="contrast">Zur Wunschliste</button></router-link>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      loggedDj: ''
+    }
+  },
+  mounted() {
+    // Beim Laden der Komponente den DJ-Namen aus dem localStorage abrufen
+    this.getLoggedDjFromLocalStorage()
+  },
+  methods: {
+    getLoggedDjFromLocalStorage() {
+      // Den DJ-Namen aus dem localStorage abrufen
+      this.loggedDj = localStorage.getItem('loggedDj')
+    }
+  }
+}
+</script>
+
+<style scoped>
+#loggedDjWelcome {
+  color: rgb(25, 255, 36);
+  font-size: 2rem;
+}
+</style>
