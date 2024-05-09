bcc95dd35e807da70874887ba67e0bf234c8b9ce
Lars Wagner <lars@tubies.de>
2024-05-09 23:02:53 +0200

    feat: add PlaylistStore, create dynamic Playlist, add complete playlist endpoint at the end

--- src/views/dj/PlaylistsView.vue
+++ src/views/dj/PlaylistsView.vue
@@ -1,12 +1,19 @@
 <template>
-  <small>PlaylistsView</small>
+  <small>
+    <active-dj><!--Zeigt aktuellen DJ an--></active-dj> @ PlaylistView</small
+  >
   <h2>Playlisten verwalten</h2>
+
   <p>Hier verwaltest du deine Playlisten</p>
   <form @submit.prevent>
     <ol>
-      <li>
+      <li v-for="playlist in playlists" :key="playlist.id">
         <details>
-          <summary role="button" class="outline contrast">Bootcamp Rock Mix</summary>
+          <summary role="button" class="outline contrast">
+            <h4>{{ playlist.title }}</h4>
+            - Event ID: {{ playlist.eventId }} - User ID: {{ playlist.userId }} - DJ ID:
+            {{ playlist.djId }}
+          </summary>
           <section class="grid">
             <input
               id="event-edit"
@@ -14,37 +21,7 @@
               type="submit"
               value="Ändern"
             />
-            <button id="event-duplicate">Duplizieren</button>
-            <button id="event-delete">Löschen</button>
-          </section>
-        </details>
-      </li>
-      <li>
-        <details>
-          <summary role="button" class="outline contrast">Schlagerparty und Ballermann</summary>
-          <section class="grid">
-            <input
-              id="event-edit"
-              @click="this.$router.push({ path: '/edit-playlist' })"
-              type="submit"
-              value="Ändern"
-            />
-            <button id="event-duplicate">Duplizieren</button>
-            <button id="event-delete">Löschen</button>
-          </section>
-        </details>
-      </li>
-      <li>
-        <details>
-          <summary role="button" class="outline contrast">Elektro, Minimal und Housemusic</summary>
-          <section class="grid">
-            <input
-              id="event-edit"
-              @click="this.$router.push({ path: '/edit-playlist' })"
-              type="submit"
-              value="Ändern"
-            />
-            <button id="event-duplicate">Duplizieren</button>
+            <!-- <button id="event-duplicate">Duplizieren</button> -->
             <button id="event-delete">Löschen</button>
           </section>
         </details>
@@ -52,4 +29,34 @@
     </ol>
   </form>
   <router-link to="/dj-overview"><button>Zurück zur Übersicht</button></router-link>
+  <hr />
+  <h3>Komplette Daten aus API inkl. Songs</h3>
+  <div v-for="playlist in playlists" :key="playlist.id">
+    <div>Title: {{ playlist.eventId }}</div>
+    <div>Event ID: {{ playlist.eventId }}</div>
+    <div>User ID: {{ playlist.userId }}</div>
+    <div>DJ ID: {{ playlist.djId }}</div>
+    <div>Songs: {{ playlist.songs }}</div>
+    <hr />
+  </div>
 </template>
+<script>
+import { usePlaylistStore } from '@/stores/PlaylistStore'
+import ActiveDj from '@/components/ActiveDj.vue'
+import { initDj } from '@/components/InitDj'
+
+export default {
+  components: { ActiveDj },
+
+  data() {
+    return {
+      playlists: []
+    }
+  },
+  async mounted() {
+    await usePlaylistStore().fetchPlaylists() // Fetch DJs data from API when component is mounted
+    this.playlists = usePlaylistStore().playlists
+    initDj()
+  }
+}
+</script>
