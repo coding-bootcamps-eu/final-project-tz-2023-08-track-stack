4e25c87987a2a641b393c23d194ae8048e41def2
Martin Schneider <schnmart86@gmail.com>
2024-05-30 20:59:25 +0200

    fix

--- src/views/WishList.vue
+++ src/views/WishList.vue
@@ -8,7 +8,6 @@
   </p>
 
   <ol>
-
     <transition-group name="wishList">
       <li v-for="request in sortedRequest" :key="request.id">
         <details name="accordion">
@@ -58,7 +57,6 @@
         </details>
       </li>
     </transition-group>
-
   </ol>
   <div class="grid">
     <div>
@@ -188,7 +186,7 @@ summary.grid {
   grid-template-columns: 1fr;
 
   @media (min-width: 768px) {
-    grid-template-columns: 2fr 1fr 1fr 1fr;
+    grid-template-columns: 2fr 1fr 1fr 1fr 1fr;
   }
 }
 hgroup {
